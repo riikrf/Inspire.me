8e31aa5028d76a1562699866fd3e32075fbf386a
Roma228337 <116820113+Belykh555@users.noreply.github.com>
2023-03-23 17:00:52 +0400

    first changes of main page

---
+++ front-end/wertyall.js
@@ -0,0 +1,80 @@
+    function good(){
+    let specy = document.getElementById("wertygood");
+    specy.style.animation = " geroin 1s backwards linear";
+    specy.style.marginLeft  = "90%";
+    specy.style.display = "inline";
+    specy.style.animationFillMode = "forwards";
+    }
+    if (specy.style.animation == "100%"){
+        specy.style.marginLeft  = "40%"
+    }
+    if (bad(specy.style.animation) == "100%" && bad(specy.style.marginLeft) == "40" ){
+        good.specy.style.animation = "0%";
+        good.specy.style.marginLeft = "90%"
+    }
+
+    function bad(){
+    let specy = document.getElementById("wertybad");
+    specy.style.animation = " geroin 1s backwards linear";
+    specy.style.marginLeft  = "90%";
+    specy.style.display = "inline";
+    specy.style.animationFillMode = "forwards";
+    }
+    if (specy.style.animation == "100%"){
+        specy.style.marginLeft  = "40%"
+    }
+
+    function our(){
+    let specy = document.getElementById("wertyour");
+    specy.style.animation = " geroin 1s backwards linear";
+    specy.style.marginLeft  = "90%";
+    specy.style.display = "inline";
+    specy.style.animationFillMode = "forwards";
+    }
+    if (specy.style.animation == "100%"){
+    specy.style.marginLeft  = "40%"
+    }
+
+    function star(){
+    let specy = document.getElementById("wertystar");
+    specy.style.animation = " geroin 1s backwards linear";
+    specy.style.marginLeft  = "90%";
+    specy.style.display = "inline";
+    specy.style.animationFillMode = "forwards";
+    }
+    if (specy.style.animation == "100%"){
+        specy.style.marginLeft  = "40%"
+    }
+
+    function se_im(){
+    let specy = document.getElementById("wertyse_im");
+    specy.style.animation = " geroin 1s backwards linear";
+    specy.style.marginLeft  = "90%";
+    specy.style.display = "inline";
+    specy.style.animationFillMode = "forwards";
+    }
+    if (specy.style.animation == "100%"){
+        specy.style.marginLeft  = "40%"
+    }
+
+    function ac_of_go(){
+    let specy = document.getElementById("wertyac_of_go");
+    specy.style.animation = " geroin 1s backwards linear";
+    specy.style.marginLeft  = "90%";
+    specy.style.display = "inline";
+    specy.style.animationFillMode = "forwards";
+    }
+    if (specy.style.animation == "100%"){
+        specy.style.marginLeft  = "40%"
+    }
+
+    function skill_dev(){
+    let specy = document.getElementById("wertyskill_dev");
+    specy.style.animation = " geroin 1s backwards linear";
+    specy.style.marginLeft  = "90%";
+    specy.style.display = "inline";
+    specy.style.animationFillMode = "forwards";
+    }
+    if (specy.style.animation == "100%"){
+        specy.style.marginLeft  = "40%"
+    }
